4ec7e365e84859c7a6f6fbe7f330c2b7a7b1a009
santipedido <pedidos028@gmail.com>
2025-07-25 12:17:21 -0500

    PingPong.vue ahora usa variable de entorno VITE_API_URL para el backend

--- frontend/src/components/PingPong.vue
+++ frontend/src/components/PingPong.vue
@@ -12,7 +12,7 @@ const response = ref('')
 
 async function pingBackend() {
   try {
-    const res = await fetch('http://localhost:8000/ping')
+    const res = await fetch(import.meta.env.VITE_API_URL + '/ping')
     const data = await res.json()
     response.value = data.message
   } catch (e) {
