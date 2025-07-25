58ca89291c33d7354ce1d5ba372a8819ec277835
santipedido <pedidos028@gmail.com>
2025-07-25 11:19:13 -0500

    Proyecto hogar geriátrico: backend FastAPI y frontend Vue.js

---
+++ frontend/src/components/PingPong.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="pingBackend">Ping</button>
+    <p v-if="response">Respuesta: {{ response }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const response = ref('')
+
+async function pingBackend() {
+  try {
+    const res = await fetch('http://localhost:8000/ping')
+    const data = await res.json()
+    response.value = data.message
+  } catch (e) {
+    response.value = 'Error de conexión'
+  }
+}
+</script> 
